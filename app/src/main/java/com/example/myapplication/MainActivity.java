ccc11c0c4a106697c068c030bc0c41b35af572fb
Mario Jose <mariofilho4k@gmail.com>
2020-09-04 00:17:15 -0300

    android primeiro exemplo botoes e textview

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,43 +1,27 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button4);
+
         Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         // The activity is created
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
-        // The activity is about to become visible.
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
-        //The activity has become visible (now it "resumes").
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
-        // Focus on another activity (this activity is about to be "stopped").
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
-        // The activity is no longer visible (now it is "stopped")
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
-        // The activity is about to be destroyed.
+
+    public void sendMessage(View view) {
+        EditText mEdit   = findViewById(R.id.editTextTextPersonName6);
+        Intent i = new Intent(this, MainActivity2.class);
+        i.putExtra("name",mEdit.getText().toString());
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle extras = getIntent().getExtras();
+        String name = extras.getString("name");
+        TextView tv2 = findViewById(R.id.textView2);
+        tv2.setText(name);
+    }
+}
