0bb41d127178e93f5fb5a4670a98bfffa465d2a4
Mario Jose <mariofilho4k@gmail.com>
2020-09-05 00:34:38 -0300

    shows gps latitude longitude with a button press and shows gravity and light intensity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -8,18 +10,23 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.TriggerEventListener;
+import android.location.Location;
 import android.os.Bundle;
 import android.hardware.TriggerEventListener;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager;
     private TriggerEventListener triggerEventListener;
     Sensor sensor;
     Sensor sensor2;
+    Sensor light;
+    Sensor magnetism;
     float x1;
     float y1;
     float z1;
@@ -32,9 +39,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button getGPSBtn =  findViewById(R.id.button2);
+        ActivityCompat.requestPermissions(MainActivity.this, new
+                String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        getGPSBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GPSTrack g = new GPSTrack(getApplicationContext());
+                Location l = g.getLocation();
+                if(l!=null)
+                {
+                    double lat = l.getLatitude();
+                    double longi = l.getLongitude();
+                    Toast.makeText(getApplicationContext(), "LAT: "+lat + "LONG: " +
+                            longi, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        magnetism = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         sensorManager.registerListener(MainActivity.this,sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(MainActivity.this,light, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(MainActivity.this,magnetism, SensorManager.SENSOR_DELAY_NORMAL);
 
 
         // The activity is created
@@ -47,17 +76,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
     @Override
     public void onSensorChanged(SensorEvent event){
-        EditText mEdit1   = findViewById(R.id.edittext1);
-        EditText mEdit2   = findViewById(R.id.edittext2);
-        EditText mEdit3   = findViewById(R.id.edittext3);
-        mEdit1.setText(""+ event.values[0]);
-        mEdit2.setText(""+ event.values[1]);
-        mEdit3.setText(""+ event.values[2]);
-        ChangeAccelerometer(event.values[0],event.values[1],event.values[2]);
-        if(CheckMovement()) {
-            Intent i = new Intent(this,MainActivity2.class);
-            startActivity(i);
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            EditText mEdit1 = findViewById(R.id.edittext1);
+            EditText mEdit2 = findViewById(R.id.edittext2);
+            EditText mEdit3 = findViewById(R.id.edittext3);
+            mEdit1.setText("" + event.values[0]);
+            mEdit2.setText("" + event.values[1]);
+            mEdit3.setText("" + event.values[2]);
+            ChangeAccelerometer(event.values[0], event.values[1], event.values[2]);
+
+            if (CheckMovement()) {
+                Intent i = new Intent(this, MainActivity2.class);
+                startActivity(i);
+            }
         }
+        Sensor sensor = event.sensor;
+        TextView textview2 = findViewById(R.id.textView5);
+        if(sensor.getType() == Sensor.TYPE_GRAVITY)
+        {
+            textview2.setText("Gravity Intensity: " + event.values[0]);
+        }
+        TextView textview = findViewById(R.id.textView4);
+        if(sensor.getType() == Sensor.TYPE_LIGHT) {
+            textview.setText("Light Intensity: " + event.values[0]);
+        }
+
     }
 
     private boolean CheckMovement(){
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
